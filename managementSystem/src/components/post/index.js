d0b551017bc7ea68fc4032c33375d962e578541f
TyroneYvesChen <243307272@qq.com>
2018-10-01 21:21:58 +0800

    '修改post样式'

--- managementSystem/src/components/post/index.js
+++ managementSystem/src/components/post/index.js
@@ -54,18 +54,18 @@ class PostForm extends React.Component {
         const formItemLayout = {
             labelCol: {
               xs: { span: 24 },
-              sm: { span: 8 },
+              sm: { span: 4 },
             },
             wrapperCol: {
               xs: { span: 24 },
-              sm: { span: 16 },
+              sm: { span: 20 },
             },
           };
 
           const formItemLayoutWithOutLabel = {
             wrapperCol: {
               xs: { span: 24, offset: 0 },
-              sm: { span: 16, offset: 8 },
+              sm: { span: 20, offset: 4 },
             },
           };
 
